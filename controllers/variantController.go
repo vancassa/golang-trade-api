4f46782a93960b9a8f18be33b8123532d3ddc301
Vanessa <vanessa.cassandra.s@gmail.com>
2024-04-27 13:17:49 +0700

    Get all variants

--- controllers/variantController.go
+++ controllers/variantController.go
@@ -110,7 +110,7 @@ func GetAllVariants(ctx *gin.Context) {
 
 	results := []models.Variant{}
 
-	err := db.Debug().Preload("Admin").Find(&results).Error
+	err := db.Debug().Preload("Product").Find(&results).Error
 	if err != nil {
 		ctx.JSON(http.StatusBadRequest, gin.H{
 			"error":   "Bad request",
