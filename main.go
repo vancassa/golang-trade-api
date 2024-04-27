6661a0b9be8ea4a3bdf0e48e9c85097829ddd940
Vanessa <vanessa.cassandra.s@gmail.com>
2024-04-27 14:26:40 +0700

    Read port according to railway docs

--- main.go
+++ main.go
@@ -1,15 +1,30 @@
 package main
 
 import (
+	"log"
+	"os"
 	"trade-api/database"
 	"trade-api/router"
 )
 
 
 func main() {
-	var PORT = ":3000"
-
 	database.StartDB()
 
-	router.StartApp().Run(PORT)
+	// Use `PORT` provided in environment or default to 3000
+  var port = envPortOr("3000")
+
+  log.Fatal(router.StartApp().Run(port))
+}
+
+// Returns PORT from environment if found, defaults to
+// value in `port` parameter otherwise. The returned port
+// is prefixed with a `:`, e.g. `":3000"`.
+func envPortOr(port string) string {
+  // If `PORT` variable in environment exists, return it
+  if envPort := os.Getenv("PORT"); envPort != "" {
+    return ":" + envPort
+  }
+  // Otherwise, return the value of `port` variable from function argument
+  return ":" + port
 }
